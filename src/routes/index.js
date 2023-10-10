24ea7faa8bfa78fe9aae7530187567107df4f433
Cael7 <carlosdomingues.ads@gmail.com>
2023-10-10 20:59:52 -0300

    Advanced Login Setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,16 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from "../pages/Welcome";
 import SignIn from "../pages/SignIn";
-import Register from "../pages/Register";
+import Register from "../pages/Registe";
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Welcome"
-        component={Welcome}
-        options={{ headerShown: false }}
-      />
+      <Stack.Screen name="Welcome" component={Welcome} />
 
-      <Stack.Screen
-        name="SignIn"
-        component={SignIn}
-        options={{ headerShown: false }}
-      />
-
-      <Stack.Screen
-        name="Register"
-        component={Register}
-        options={{ headerShown: false }}
-      />
+      <Stack.Screen name="SignIn" component={SignIn} />
     </Stack.Navigator>
   );
 }
