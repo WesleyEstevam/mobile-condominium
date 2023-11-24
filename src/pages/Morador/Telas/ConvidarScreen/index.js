66c93fa07e2077fce060f3614ba60b3266777b53
Cael7 <carlosdomingues.ads@gmail.com>
2023-11-23 22:07:40 -0300

    build_screen_conviteScreen_over

--- src/pages/Morador/Telas/ConvidarScreen/index.js
+++ src/pages/Morador/Telas/ConvidarScreen/index.js
@@ -4,10 +4,12 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  StyleSheet,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
+import * as Animatable from "react-native-animatable";
 import styles from "../ConvidarScreen/style";
 
 export default function ConvidarScreen() {
@@ -19,7 +21,31 @@ export default function ConvidarScreen() {
   const [nomeConvidado, setNomeConvidado] = useState("");
   const [documento, setDocumento] = useState("");
 
+  const [apartamentoExampleVisible, setApartamentoExampleVisible] =
+    useState(true);
+  const [complementoExampleVisible, setComplementoExampleVisible] =
+    useState(true);
+  const [telefoneExampleVisible, setTelefoneExampleVisible] = useState(true);
+  const [nomeConvidadoExampleVisible, setNomeConvidadoExampleVisible] =
+    useState(true);
+  const [documentoExampleVisible, setDocumentoExampleVisible] = useState(true);
+
   const handleSave = () => {
+    if (!apartamento || !telefone || !nomeConvidado || !documento) {
+      Alert.alert("Preencha todos os campos corretamente");
+      return;
+    }
+
+    if (telefone.length !== 11) {
+      Alert.alert("Preencha o campo de telefone corretamente");
+      return;
+    }
+
+    if (documento.length !== 14) {
+      Alert.alert("Preencha o campo de documento corretamente");
+      return;
+    }
+
     console.log("Apartamento:", apartamento);
     console.log("Complemento:", complemento);
     console.log("Telefone:", telefone);
@@ -29,68 +55,135 @@ export default function ConvidarScreen() {
     navigation.goBack();
   };
 
+  const formatPhoneNumber = (text) => {
+    // Remove characters that are not digits
+    const cleanedText = text.replace(/\D/g, "");
+
+    // Check if it's a valid length for a phone number
+    if (cleanedText.length >= 2) {
+      // Format the first two digits within parentheses
+      setTelefone(`(${cleanedText.slice(0, 2)}) ${cleanedText.slice(2)}`);
+    } else {
+      setTelefone(cleanedText);
+    }
+  };
+
+  const formatCPF = (text) => {
+    // Remove characters that are not digits
+    const cleanedText = text.replace(/\D/g, "");
+
+    // Check if it's a valid length for a CPF
+    if (cleanedText.length >= 3) {
+      // Format the first three digits with a dot
+      setDocumento(`${cleanedText.slice(0, 3)}.`);
+    }
+
+    if (cleanedText.length >= 6) {
+      // Format the next three digits with a dot
+      setDocumento(`${cleanedText.slice(3, 6)}.`);
+    }
+
+    if (cleanedText.length >= 9) {
+      // Format the next three digits with a dash
+      setDocumento(`${cleanedText.slice(6, 9)}-`);
+    }
+
+    if (cleanedText.length >= 11) {
+      // Add the remaining digits
+      setDocumento(`${cleanedText.slice(9)}`);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Apartamento:</Text>
-        <View style={styles.inputWrapper}>
-          <TextInput
-            style={styles.input}
-            value={apartamento}
-            onChangeText={(text) => setApartamento(text)}
-          />
-        </View>
-      </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <Animatable.View
+        animation="fadeInLeft"
+        delay={600}
+        style={styles.containerHeader}
+      >
+        <Text style={styles.message}>Convidado</Text>
+      </Animatable.View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Complemento:</Text>
-        <View style={styles.inputWrapper}>
-          <TextInput
-            style={styles.input}
-            value={complemento}
-            onChangeText={(text) => setComplemento(text)}
-          />
+      <View style={styles.containerForm}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>*Apartamento:</Text>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              style={styles.input}
+              value={apartamento}
+              onChangeText={(text) => setApartamento(text)}
+              onFocus={() => setApartamentoExampleVisible(false)}
+              onBlur={() => setApartamentoExampleVisible(true)}
+              placeholder={apartamentoExampleVisible ? "Ex: 123" : ""}
+            />
+          </View>
         </View>
-      </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Telefone:</Text>
-        <View style={styles.inputWrapper}>
-          <TextInput
-            style={styles.input}
-            value={telefone}
-            onChangeText={(text) => setTelefone(text)}
-            keyboardType="phone-pad"
-          />
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Complemento:</Text>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              style={styles.input}
+              value={complemento}
+              onChangeText={(text) => setComplemento(text)}
+              onFocus={() => setComplementoExampleVisible(false)}
+              onBlur={() => setComplementoExampleVisible(true)}
+              placeholder={complementoExampleVisible ? "Ex: Apto 2" : ""}
+            />
+          </View>
         </View>
-      </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Nome do Convidado:</Text>
-        <View style={styles.inputWrapper}>
-          <TextInput
-            style={styles.input}
-            value={nomeConvidado}
-            onChangeText={(text) => setNomeConvidado(text)}
-          />
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>*Telefone:</Text>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              style={styles.input}
+              value={telefone}
+              onChangeText={(text) => formatPhoneNumber(text)}
+              keyboardType="phone-pad"
+              onFocus={() => setTelefoneExampleVisible(false)}
+              onBlur={() => setTelefoneExampleVisible(true)}
+              placeholder={telefoneExampleVisible ? "Ex: (11) 91234-5678" : ""}
+            />
+          </View>
         </View>
-      </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Documento:</Text>
-        <View style={styles.inputWrapper}>
-          <TextInput
-            style={styles.input}
-            value={documento}
-            onChangeText={(text) => setDocumento(text)}
-            keyboardType="numeric" // Use "numeric" para exibir apenas o teclado numérico
-          />
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>*Nome do Convidado:</Text>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              style={styles.input}
+              value={nomeConvidado}
+              onChangeText={(text) => setNomeConvidado(text)}
+              onFocus={() => setNomeConvidadoExampleVisible(false)}
+              onBlur={() => setNomeConvidadoExampleVisible(true)}
+              placeholder={nomeConvidadoExampleVisible ? "Ex: João Silva" : ""}
+            />
+          </View>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>*Documento:</Text>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              style={styles.input}
+              value={documento}
+              onChangeText={(text) => formatCPF(text)}
+              keyboardType="numeric"
+              onFocus={() => setDocumentoExampleVisible(false)}
+              onBlur={() => setDocumentoExampleVisible(true)}
+              placeholder={documentoExampleVisible ? "Ex: 123.456.789-01" : ""}
+            />
+          </View>
         </View>
       </View>
 
       <TouchableOpacity style={styles.button} onPress={handleSave}>
         <Text style={styles.buttonText}>Convidar</Text>
       </TouchableOpacity>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
--- src/pages/Morador/Telas/ConvidarScreen/style.jsx
+++ src/pages/Morador/Telas/ConvidarScreen/style.jsx
@@ -1,38 +1,107 @@
 import { StyleSheet } from "react-native";
-import commonStyles from "../../style";
 
-const localStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    justifyContent: "center",
+    backgroundColor: "green",
+    width: "100%",
+    justifyContent: "left", 
+    alignItems: "left", 
+    
   },
-  inputContainer: {
-    marginBottom: 16,
+  containerHeader: {
+    marginTop: "10%",
+    marginBottom: "2%",
+    paddingStart: "5%",
+    alignItems: "flex-start",
   },
-  label: {
-    fontSize: 18,
-    marginBottom: 8,
+  message: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 20,
+    marginLeft: 20,
+
   },
-  inputWrapper: {
-    borderBottomWidth: 1,
-    borderColor: "gray",
+  containerForm: {
+    backgroundColor: "white",
+    flex: 8,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: "5%",
+    paddingEnd: "5%",
+    width: "100%",
+    alignItems: "left", 
+    
+  },
+  title: {
+    fontSize: 20,
+    marginTop: 28,
+    marginBottom: 10,
   },
   input: {
-    height: 40,
-    paddingHorizontal: 8,
+    
+    borderBottomWidth: 1,
+    height: 30,
+    marginBottom: 20,
     fontSize: 16,
+    width: "100%", // Take the full width
   },
   button: {
     backgroundColor: "green",
-    padding: 12,
+    width: "100%",
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginTop: 10,
+    justifyContent: "center",
     alignItems: "center",
-    borderRadius: 8,
   },
   buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  errorText: {
+    color: "red",
+    marginTop: 5,
+  },
+  backButton: {
+    marginTop: 14,
+  },
+  backButtonText: {
+    fontWeight: "bold",
+    fontSize: 18,
+    alignSelf: "flex-start",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  segmentedControl: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 10,
+    width: "100%",
+  },
+  segmentedControlItem: {
+    flex: 1,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 8,
+    alignItems: "center",
+  },
+  segmentedControlText: {
+    color: "black",
+  },
+  segmentedControlItemSelected: {
+    backgroundColor: "green",
+  },
+  segmentedControlTextSelected: {
     color: "white",
+  },
+  label: {
+    marginTop: 30,
     fontSize: 18,
   },
 });
 
-export default { ...commonStyles, ...localStyles };
+export default styles;
